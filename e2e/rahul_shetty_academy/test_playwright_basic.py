ec7c6336c02da8b6cd45f2047f4f5749757b27a1
Roman Lagunov <rlagunov@gmail.com>
2025-09-08 16:28:14 -0700

    add -headed handler

---
+++ e2e/rahul_shetty_academy/test_playwright_basic.py
@@ -0,0 +1,17 @@
+import time
+from playwright.sync_api import Playwright
+
+def test_playwright_basic(playwright: Playwright,pytestconfig):
+    headed = pytestconfig.getoption("--headed")
+    browser = playwright.chromium.launch(headless=not headed)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+    page.get_by_label('Username:').fill('rahulshettyacademy')
+    page.get_by_label('Password:').fill('learning')
+    page.get_by_role('combobox').select_option('teach')
+    page.locator('#terms').check()
+    page.get_by_role('link',name='terms and conditions').click()
+    page.get_by_role('button',name='Sign In').click()
+    time.sleep(10)
+    
